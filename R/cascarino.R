553500ba5c6c84c994429733a6c75b99ca6b9e37
Torvaney <btorvaney@gmail.com>
2017-11-22 18:25:29 +0000

    More trivia calcs

--- R/cascarino.R
+++ R/cascarino.R
@@ -17,13 +17,13 @@ teams_of_interest <- c("Man City", "Celtic")
 
 # Plot ppg histograms ----
 simulations %>% 
-  mutate(played = ifelse(country == "Scotland", 44, 46),
-         ppg = points / played) %>% 
+  mutate(played = win + draw + lose,
+         ppg = points / played) %>%
   filter(team %in% teams_of_interest) %>% 
   ggplot(aes(x = ppg)) +
-  # geom_histogram(aes(y = 100 * ..count.. / 1e4), 
-  #                binwidth = 0.1, 
-  #                fill = colours$high) +
+  # geom_histogram(aes(y = 100 * ..count.. / 1e4,
+  #                    fill = team),
+  #                binwidth = 0.1) +
   geom_density(aes(y = 10 * ..count.. / 1e4,
                    fill = team),
                colour = NA,
@@ -39,14 +39,27 @@ simulations %>%
   ylab("%")
 
 
-strengths %>% 
-  filter(season == 2017) %>% 
-  ggplot(aes(x = reorder(team, strength), y = strength)) +
-  # geom_point(aes()) +
+strengths %>%
+  filter(season == 2017,
+         team %in% unique(simulations$team)) %>% 
+  group_by(country) %>% 
+  mutate(strength_adj = strength - mean(strength)) %>% 
+  ggplot(aes(x = reorder(team, strength_adj), y = strength_adj)) +
+  geom_hline(yintercept = 0, colour = colours$low, linetype = "dotted") +
   geom_segment(aes(y = lower, yend = upper,
-                   xend = reorder(team, strength))) +
+                   xend = reorder(team, strength_adj)),
+               colour = colours$high) +
   facet_wrap( ~ country, scales = "free_y") +
-  coord_flip()
+  coord_flip() +
+  theme_minimal() +
+  theme(legend.position = "None",
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(linetype = "dotted")) +
+  ggtitle("Relative team strengths", 
+          "95% credible intervals") +
+  ylab("Relative strength") +
+  xlab("")
 
 # Trivia ----
 simulations %>% 
@@ -63,6 +76,32 @@ simulations %>%
   group_by(team) %>% 
   mutate(p = n / sum(n))
 
+simulations %>% 
+  mutate(tie_break = runif(nrow(simulations))) %>%
+  ungroup() %>% 
+  group_by(country, sim_id) %>% 
+  arrange(sim_id, country, desc(points)) %>% 
+  mutate(r = row_number(),
+         champion = r == 1) %>%
+  filter(team %in% teams_of_interest) %>% 
+  group_by(team, champion) %>% 
+  summarise(n = n()) %>% 
+  group_by(team) %>% 
+  mutate(p = n / sum(n))
+
+simulations %>%
+  mutate(tie_break = runif(nrow(simulations))) %>%
+  ungroup() %>%
+  group_by(country, sim_id) %>% 
+  arrange(sim_id, country, desc(points), tie_break) %>% 
+  mutate(r = row_number(),
+         champion = r == 1) %>% 
+  filter(champion,
+         !(team %in% teams_of_interest)) %>% 
+  group_by(team, champion) %>% 
+  summarise(n = n()) %>% 
+  ungroup() %>% 
+  mutate(p = n / sum(n))
 
 # Sandbox ----
 simulations %>% 
